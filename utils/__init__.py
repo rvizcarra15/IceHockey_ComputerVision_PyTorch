a432b2ae549e102dc50f9c1929a531686271ecde
Raul Vizcarra Chirinos <72183583+rvizcarra15@users.noreply.github.com>
2024-07-09 16:49:40 -0500

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  2 19:39:08 2024
-
-@author: saga
-"""
-
-from .video_utils import read_video, save_video
-from .bbox_utils import get_center_of_bbox, get_bbox_width
